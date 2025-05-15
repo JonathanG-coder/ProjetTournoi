f40f2f195bc3b6eee4903b5c50fe0512f5ee8b24
Damien Di Luca <damiendiluca@MacBook-Air-de-Damien.local>
2025-05-15 13:29:01 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -1,18 +1,80 @@
-export default function createTournament() {
-    const tournament = document.createElement('div')
-    tournament.className = "tournament"
+import  createFooter  from "/components/footer/footer.js";
+import  createHeader  from "/components/header/header.js";
 
-const response = fetch("./data/tournaments.json") // Nous avons fait appel à tout l'objet JSON (skills.json)
-    .then(response => response.json()) // Nous avons converti la réponse en un format que le navigateur connait (JSON)
-    .then(data => tournament.innerHTML = // Nous avons parcouru la réponse et affiché grâce à innerHTML, les éléments de l'objet
-        data.map(tournament => `
-            <h1>${tournament.name}</h1>
-            <h3>${tournament.game}</h3>
-            <p>${tournament.date}</p>
-            <p>${tournament.maxParticipants}</p>
-            <p>${tournament.currentParticipants}</p>
-            `).join("")
-        );
-    
-        return tournament // Nous avons retourné la div pour être utilisée ailleurs
+const header = createHeader()
+document.body.appendChild(header)
+
+const footer = createFooter()
+document.body.appendChild(footer)
+
+const nav = document.createElement('navbar')
+function createNavbar(navbar) {
+    nav.innerHTML = `
+    <button id="theme-toggle" class="themeBtn">🌙</button>
+    <nav class="navbar bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+  <div class="form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="switchCheckDefault">
+  </div>
+    <a class="navbar-brand" href="#">🎮 Tournoigame 🏆</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Qui suis-je ?</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mes compétences</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mon portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contactez-moi</a>
+          </li>
+        <form class="d-flex mt-3" role="Rechercher">
+          <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Rechercher"/>
+          <button class="btn btn-outline-success" type="submit">Rechercher</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
+    `
+    document.body.appendChild(nav)
+    return nav
 }
+
+const containerTournaments = document.getElementById(`tournaments`);
+
+    function afficherTournaments(tournaments) {
+        tournaments.forEach((tournament) => {
+          const divTournaments = document.createElement(`div`);
+          divTournaments.innerHTML = `
+          <img src="${tournament.img}">
+          <h1>${tournament.name}</h1>
+          <h2>${tournament.game}</h2>
+          <p>${tournament.date}</p>
+          <p>${tournament.maxParticipants}</p>
+          <p>${tournament.currentParticipants}</p>
+          <input type="button" Value="S'inscrire" onclick="window.location='/pages/inscription.html';">
+          `;
+          containerTournaments.appendChild(divTournaments);
+        });
+      }
+
+fetch(`/data/tournaments.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      afficherTournaments(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
